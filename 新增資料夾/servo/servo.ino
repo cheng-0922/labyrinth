8d1bfa0588f6e7203e2d10794eba08fcdaa243d4
chosenone613 <lminyan1997@gmail.com>
2026-05-23 12:30:48 +0800

    upgrade control

--- 新增資料夾/servo/servo.ino
+++ 新增資料夾/servo/servo.ino
@@ -12,11 +12,9 @@ const int joyYPin = A7;
 const int center = 512;
 const int deadZone = 50;
 
-// 伺服馬達的基準角度
-const int baseAngleX = 90;
-const int baseAngleY = 95;
+const int baseAngleX = 97;
+const int baseAngleY = 90;
 
-// 限制在基準點正負 10 度
 const int swingAngle = 10;
 
 const int minAngleX = baseAngleX - swingAngle;
@@ -25,21 +23,41 @@ const int maxAngleX = baseAngleX + swingAngle;
 const int minAngleY = baseAngleY - swingAngle;
 const int maxAngleY = baseAngleY + swingAngle;
 
-// 目前伺服馬達角度
 float angleX = baseAngleX;
 float angleY = baseAngleY;
 
-// 最大轉動速度
 const float maxSpeed = 0.4;
-
-// 更新間隔，越大越慢
 const int moveDelay = 20;
+
+float joystickToSpeed(int offset) {
+  if (abs(offset) <= deadZone) {
+    return 0;
+  }
+
+  /*
+  // Original method: speed increases linearly after leaving the dead zone.
+  int direction = offset > 0 ? 1 : -1;
+  int effectiveOffset = abs(offset) - deadZone;
+  int effectiveRange = 512 - deadZone;
+
+  float ratio = effectiveOffset / float(effectiveRange);
+  return direction * ratio * maxSpeed;
+  */
+
+  int direction = offset > 0 ? 1 : -1;
+  int effectiveOffset = abs(offset) - deadZone;
+  int effectiveRange = 512 - deadZone;
+
+  float ratio = effectiveOffset / float(effectiveRange);
+  ratio = ratio * ratio;
+
+  return direction * ratio * maxSpeed;
+}
 
 void setup() {
   servoX.attach(servoXPin);
   servoY.attach(servoYPin);
 
-  // 開機時先讓伺服馬達回到基準角
   servoX.write(baseAngleX);
   servoY.write(baseAngleY);
 
@@ -47,29 +65,18 @@ void setup() {
 }
 
 void loop() {
-
-  // 平均濾波使讀值較穩定
   int joyX = (analogRead(joyXPin) + analogRead(joyXPin) + analogRead(joyXPin)) / 3;
   int joyY = (analogRead(joyYPin) + analogRead(joyYPin) + analogRead(joyYPin)) / 3;
-  
+
   int offsetX = joyX - center;
   int offsetY = joyY - center;
 
-  float speedX = 0;
-  float speedY = 0;
-
-  if (abs(offsetX) > deadZone) {
-    speedX = map(offsetX, -512, 511, -maxSpeed * 100, maxSpeed * 100) / 100.0;
-  }
-
-  if (abs(offsetY) > deadZone) {
-    speedY = map(offsetY, -512, 511, -maxSpeed * 100, maxSpeed * 100) / 100.0;
-  }
+  float speedX = joystickToSpeed(offsetX);
+  float speedY = joystickToSpeed(offsetY);
 
   angleX += speedX;
   angleY -= speedY;
 
-  // 角度限制在「伺服馬達基準點 ± 10 度」
   angleX = constrain(angleX, minAngleX, maxAngleX);
   angleY = constrain(angleY, minAngleY, maxAngleY);
 
